1158415bca7bb12f371364993be1ce51f79fe735
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2023-11-24 19:38:15 +0700

    feat: add wifi-ext.sh for reference

---
+++ wifi-ext/wifi-ext.sh
@@ -0,0 +1,71 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Tested on Redmi 4x with latest MIUI (rooted)
+
+# Requires: libubox, relayd (compiled from openwrt git source)
+
+# https://android.stackexchange.com/a/202335
+# https://forums.raspberrypi.com/viewtopic.php?t=1763 
+# https://clockworkbird9.wordpress.com/2020/02/16/install-libubox-and-ubus-on-ubuntu/
+# https://forum.fairphone.com/t/rom-unofficial-lineageos-16-0-for-fp3/59849/218?page=12
+
+set -e
+set -o pipefail
+set -x
+
+WIFI_IF=wlan0
+SSID=myssid
+PASS=mypassword
+
+if [ "$(id -u)" != 0 ]; then
+	echo 'Need root!'
+	exit 1
+fi
+
+AP_IF=$WIFI_IF-ap	# $AP_IF must be $WIFI_IF-ap! 
+WIFI_SUBNET=$(ip route | grep wlan0 | cut -d' ' -f1)
+
+cleanup()
+{
+	iptables -D FORWARD -i $WIFI_IF -j ACCEPT
+	iptables -D FORWARD -i $AP_IF -j ACCEPT
+	echo 0 > /proc/sys/net/ipv4/ip_forward
+	echo 0 > /proc/sys/net/ipv4/conf/$WIFI_IF/arp_filter
+	echo 0 > /proc/sys/net/ipv4/conf/$AP_IF/arp_filter
+        iw $AP_IF del
+        rm -rf $SSID
+}
+
+trap cleanup EXIT
+
+iw dev $WIFI_IF interface add $AP_IF type __ap
+ip link set up dev $AP_IF
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv4/conf/$WIFI_IF/arp_filter
+echo 1 > /proc/sys/net/ipv4/conf/$AP_IF/arp_filter
+iptables -I FORWARD -i $AP_IF -j ACCEPT
+iptables -I FORWARD -i $WIFI_IF -j ACCEPT
+
+mkdir -p $SSID
+cat << EOF > $SSID/hostapd.conf
+# network name
+ssid=$SSID
+# network interface to listen on
+interface=$AP_IF
+# wi-fi driver
+driver=nl80211
+# WLAN channel to use
+channel=11
+# ser operation mode, what frequency to use
+hw_mode=g
+# enforce Wireless Protected Access (WPA)
+wpa=2
+# passphrase to use for protected access
+wpa_passphrase=$PASS
+# WPA protocol
+wpa_key_mgmt=WPA-PSK
+EOF
+
+(cd $SSID && hostapd hostapd.conf)&
+
+relayd -I $WIFI_IF -I $AP_IF -D -B -d
